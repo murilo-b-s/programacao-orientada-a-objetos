42d56e4f3e23a00c5de9146b870fd5deade8e537
Murilo Brum Sena <165914609+murilo-b-s@users.noreply.github.com>
2024-10-03 11:24:33 -0300

    herança em vários arquivos

---
+++ aula7/projeto_heranca/main.py
@@ -0,0 +1,12 @@
+from cachorro import Cachorro
+from gato import Gato
+from passaro import Passaro
+
+
+cachorro = Cachorro("Rex", 3, "Labrador")
+gato = Gato("Mia", 2, "Branco")
+passaro = Passaro("Piu-Piu", 1, "Canario")
+
+cachorro.emitir_som()
+gato.emitir_som()
+passaro.emitir_som()
