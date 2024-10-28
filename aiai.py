f46df1cd07d5d75298461e11f538b75be1810460
Murilo Brum Sena <murilobrumsena08@gmail.com>
2024-10-27 23:45:19 -0300

    atv avaliativa 3

--- aiai.py
+++ aiai.py
@@ -1,24 +1,32 @@
-import mysql.connector
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Conecte ao banco de dados
-conexao = mysql.connector.connect(
-    host="localhost",
-    user="root",  # Usuário padrão do MySQL no XAMPP
-    password="",  # Geralmente a senha padrão é vazia
-    database="meu_banco"  # Nome do seu banco de dados
-)
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost/locadora_carros"
 
-# Crie um cursor para executar consultas SQL
-cursor = conexao.cursor()
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 
-# Exemplo de uma consulta SQL para obter dados de uma tabela
-cursor.execute("SELECT * FROM sua_tabela")
+class Locadora(Base):
+    __tablename__ = 'Locadora'
 
-# Mostre os resultados
-resultados = cursor.fetchall()
-for linha in resultados:
-    print(linha)
+    ID_veiculo = Column(Integer, primary_key=True, autoincrement=True)
+    marca = Column(String(50), nullable=False)
+    modelo = Column(String(50), nullable=False)
+    placa = Column(String(50), nullable=False)
+    ano = Column(Integer, nullable=False)
+    valor_diaria = Column(Integer, nullable=False)
 
-# Feche a conexão ao finalizar
-cursor.close()
-conexao.close()
+def inserir(marca, modelo, placa, ano, valor_Diaria):
+    new_Car = Locadora(marca=marca, modelo=modelo, placa=placa, ano=ano, valor_Diaria=valor_Diaria)
+    session.add(Locadora)
+    session.commit()
+    print(f'O carro {marca} adicionado com sucesso')
+
+'''ID_veiculo (INT, PRIMARY KEY, AUTO_INCREMENT): Identificador único do veículo.
+marca (VARCHAR): Marca do veículo.
+modelo (VARCHAR): Modelo do veículo.
+placa (VARCHAR): Placa do veículo.
+ano (INT): Ano de fabricação do veículo.
+valor_diaria (DECIMAL): Valor da diária do veículo.'''
